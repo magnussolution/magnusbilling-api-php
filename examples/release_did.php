417152a0d95dd6c98daa86b57a23bd123f6c613b
MagnusSolution <adilsom81@yahoo.com.br>
2023-08-07 11:24:18 -0300

    Update release_did.php

--- examples/release_did.php
+++ examples/release_did.php
@@ -8,6 +8,6 @@ $magnusBilling = new MagnusBilling('API_KEY', 'SECRET_KEY');
 // Your MagnusBilling URL
 $magnusBilling->public_url = "http://x.x.x.x/mbilling";
 
-$result = $magnusBilling->releaseDID('343434');
+$result = $magnusBilling->releaseDID('DID_NUMBER');
 
 print_r($result);
